0e5c6faccae05cce1083d2613efd77bce90928b7
曹珂俭 <1849201815@qq.com>
2023-06-11 21:29:31 +0800

    使用redux-thunk来异步派发

--- src/share/redux/category.jsx
+++ src/share/redux/category.jsx
@@ -2,19 +2,20 @@ import axios from 'axios'
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux';
 import store from '../../store'
-import { changeBannersAction, changeRecommendsAction } from '../../store/actionCreators'
+import { changeBannersAction, changeRecommendsAction, fetchHomeMultidataAction } from '../../store/actionCreators'
 
 export class Category extends PureComponent {
   componentDidMount() {
-    axios.get("http://123.207.32.32:8000/home/multidata").then(res => {
-      const banners = res.data.data.banner.list
-      const recommends = res.data.data.recommend.list
-      console.log(banners, recommends);
-      // store.dispatch(changeBannersAction(banners))
-      // store.dispatch(changeRecommendsAction(recommends))
-      this.props.changeBanners(banners)
-      this.props.changeRecommends(recommends)
-    })
+    // axios.get("http://123.207.32.32:8000/home/multidata").then(res => {
+    //   const banners = res.data.data.banner.list
+    //   const recommends = res.data.data.recommend.list
+    //   console.log(banners, recommends);
+    //   // store.dispatch(changeBannersAction(banners))
+    //   // store.dispatch(changeRecommendsAction(recommends))
+    //   this.props.changeBanners(banners)
+    //   this.props.changeRecommends(recommends)
+    // })
+    this.props.fetchHomeList()
   }
   render() {
     return (<div>
@@ -25,14 +26,15 @@ export class Category extends PureComponent {
   }
 }
 
-function mapDispatchProps(dispatch) {
-  return {
-    changeBanners(banners) {
-      dispatch(changeBannersAction(banners))
-    },
-    changeRecommends(recommends) {
-      dispatch(changeRecommendsAction(recommends))
+const mapDispatchProps = (dispatch) => ({
+    // changeBanners(banners) {
+    //   dispatch(changeBannersAction(banners))
+    // },
+    // changeRecommends(recommends) {
+    //   dispatch(changeRecommendsAction(recommends))
+    // }
+    fetchHomeList(){
+      dispatch(fetchHomeMultidataAction())
     }
-  }
-}
+})
 export default connect(null, mapDispatchProps)(Category);
--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './constants';
 export const addNumberAction = (num) => ({
   type: actionTypes.ADD_NUMBER,
@@ -17,3 +18,14 @@ export const changeRecommendsAction = (recommends) => ({
   type: actionTypes.CHANGE_RECOMMENDS,
   recommends
 })
+
+export const fetchHomeMultidataAction = () => {
+  return function foo(dispatch, getState) {
+    axios.get("http://123.207.32.32:8000/home/multidata").then(res => {
+      const banners = res.data.data.banner.list
+      const recommends = res.data.data.recommend.list
+      dispatch(changeBannersAction(banners))
+      dispatch(changeRecommendsAction(recommends))
+    })
+  }
+}
